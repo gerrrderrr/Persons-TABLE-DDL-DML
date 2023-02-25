f4171c2f01d38865f97ba93589a3b3074fc0c4ce
Olga Kozlova <107241297+gerrrderrr@users.noreply.github.com>
2023-02-25 16:54:14 +0300

    Added file to add peope in the existing table

---
+++ insert_people.sql
@@ -0,0 +1,6 @@
+INSERT INTO netology.PERSONS
+values ('Anna', 'Smith', 23, 79998887766, 'MOSCOW'),
+       ('Brian', 'Warner', 44, 76665554433, 'Night City'),
+       ('Vi', 'Unknown', 25, 74443332211, 'Owl Post'),
+       ('Tomas', 'Edison', 67, 71112223344, 'MOSCOW'),
+       ('Arthur', 'Morgan', 36, 74446669900, 'REMEMBERED');
